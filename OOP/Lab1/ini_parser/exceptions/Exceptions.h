e86a7bc49f0cc1302c532600d49be28ab784fac2
Nikolay Fedotenko <vafedotenko@gmail.com>
2020-10-01 21:29:28 +0300

    Delete Lab1

--- OOP/Lab1/ini_parser/exceptions/Exceptions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "DuplicateOptionException.h"
-#include "DuplicateSectionException.h"
-#include "INIFormatException.h"
-#include "INIReaderException.h"
-#include "InvalidOptionException.h"
-#include "InvalidSectionException.h"
-#include "InvalidTypeException.h"
